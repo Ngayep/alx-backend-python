ed42d7a91faf9b255ed571b85ba152e4b18a5df3
Ngayep <courtesyofjess@gmail.com>
2024-10-15 13:14:14 +0100

    elementlenght

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-from typing import List, Tuple, Any
+from typing import List, Tuple
 
 
-def element_length(lst: List[Any]) -> List[Tuple[Any, int]]:
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
     """Return a list of tuples containing elements and their lengths.
 
     Args:
-        lst (List[Any]): A list of elements (can be of any type).
+        lst (List[str]): A list of strings.
 
     Returns:
-        List[Tuple[Any, int]]: A list of tuples, where each tuple contains
-        an element and its length (if applicable).
+        List[Tuple[str, int]]: A list of tuples, where each tuple contains
+        an element and its length.
     """
-    return [(i, len(i)) for i in lst if isinstance(i, (str, bytes))]
+    return [(i, len(i)) for i in lst]
