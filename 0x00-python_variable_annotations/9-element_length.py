79ca59a90454eacf698a4b3c429b031490376d7e
Ngayep <courtesyofjess@gmail.com>
2024-10-15 13:29:07 +0100

    elementlenght

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-from typing import List, Tuple
+from typing import List, Tuple, Sequence
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Sequence) -> List[Tuple[Sequence, int]]:
     """Return a list of tuples containing elements and their lengths.
 
     Args:
-        lst (List[str]): A list of strings.
+        lst (Sequence): A sequence (like a list or a string) of elements.
 
     Returns:
-        List[Tuple[str, int]]: A list of tuples, where each tuple contains
+        List[Tuple[Sequence, int]]: A list of tuples,
+        where each tuple contains
         an element and its length.
     """
     return [(i, len(i)) for i in lst]
