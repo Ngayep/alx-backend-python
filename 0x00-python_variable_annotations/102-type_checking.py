e67cad7b9822079501071c4aefaeb9bbe8cc4875
Ngayep <courtesyofjess@gmail.com>
2024-10-15 15:16:55 +0100

    102

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,24 @@
+from typing import List, Tuple
+
+def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
+    """Zoom in on the given array by a specified factor.
+
+    Args:
+        lst (List[int]): The input array to zoom.
+        factor (int): The zoom factor. Default is 2.
+
+    Returns:
+        List[int]: The zoomed-in array.
+    """
+    zoomed_in: List[int] = [
+        item for item in lst
+        for _ in range(factor)
+    ]
+    return zoomed_in
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+# Change the factor to an integer
+zoom_3x = zoom_array(array, 3)  # Use an integer for the factor
