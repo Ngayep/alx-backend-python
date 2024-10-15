88d7d4418ba7757ce4b498e7185317763c51af6b
Ngayep <courtesyofjess@gmail.com>
2024-10-15 10:49:50 +0100

    add

---
+++ 0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A type-annotated function that returns the sum of two floats."""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function that returns the sum of its arguments as a float.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
